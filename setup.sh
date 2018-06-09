762aaa2837632552dbea8afc2c8ad087deed5160
Andrew Jarombek <ajarombek95@gmail.com>
2018-06-09 15:44:07 -0400

    Sending Emails with CSS Inlined and Images

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ yarn init
 
 # NodeJS module for sending emails
 yarn add nodemailer
+
+# Dependency for inlining CSS in HTML
+yarn add juice
--- welcomeEmail.js
+++ welcomeEmail.js
@@ -6,8 +6,19 @@
  */
 
 const nodemailer = require('nodemailer');
+const fs = require('fs');
+const juice = require('juice');
 
-const to = 'abjaro13@stlawu.edu';
+const to = 'andrew@jarombek.com';
+
+// Read the contents of the HTML and CSS files to send with the email
+const html = fs.readFileSync('./welcomeEmail.html', 'utf8');
+const css = fs.readFileSync('./welcomeEmail.css', 'utf8');
+
+// Inline the CSS styles in the HTML document
+const styledHtml = juice.inlineContent(html, css);
+
+console.info(styledHtml);
 
 const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -24,11 +35,27 @@ transport.sendMail({
     from: 'andrew@jarombek.com',
     to,
     subject: 'Andrew Jarombek\'s Software Development Blog Subscription',
-    text: '<div>Hello World</div>',
+    html: styledHtml,
+    attachments: [
+        {
+            filename: 'blizzard.png',
+            path: './assets/blizzard.png',
+            cid: 'background@jarombek.com'
+        },
+        {
+            filename: 'jarombek.png',
+            path: './assets/jarombek.png',
+            cid: 'jarombek@jarombek.com'
+        }
+    ],
     auth: {
         user: 'andrew@jarombek.com',
         refreshToken: '1/sysZin_KovuBXt2mn2OcrfEtv-haLlHSgXZANHyJLlU',
         accessToken: 'ya29.GlvSBV0rBnphJNMZc2PaszNLBCdh0EOQPzmWcXbcsS1i_3SQkRSFMYYB1ul6eeIg2LTBRIZYDSviqb_WE0nfbUcJSW0Af9Q9cLsYsGbyoCey41K0fY6pywAMFxIb',
         expires: 3600
+    }
+}, (err) => {
+    if (err) {
+        console.error(`Something went wrong sending mail: ${err}`);
     }
 });
