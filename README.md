5bd9da7b2d5800aaf3c546d3e7bbfbb06b958e0e
Andrew Jarombek <ajarombek95@gmail.com>
2020-05-23 17:46:53 -0400

    send email using secrets manager

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # jarombek-com-emails
+
+> *Formerly jarombek-com-emails*
+
+### Overview
+
+### Commands
+
+```bash
+# Create an AWS Lambda ZIP file.
+rm welcomeEmail.zip
+zip -r welcomeEmail.zip .
+```
+
+### Resources
+
+**Outdated Resources**
+
+1. [NodeJS Gmail OAuth2](http://nodemailer.blogspot.com/)
+2. [NodeMailer Auth](https://nodemailer.com/smtp/oauth2/#example-5)
+3. [NodeMailer Refresh Token](https://stackoverflow.com/questions/24098461/nodemailer-gmail-what-exactly-is-a-refresh-token-and-how-do-i-get-one)
+
+**Current Resources**
+
+1. [Get SecretsManager Secret](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SecretsManager.html#getSecretValue-property)
--- welcomeEmail.js
+++ welcomeEmail.js
@@ -2,22 +2,18 @@
  * Send a welcome Email when someone subscribes
  * @author Andrew Jarombek
  * @since 6/5/2018
- * @sources [
- *      http://nodemailer.blogspot.com/,
- *      https://nodemailer.com/smtp/oauth2/#example-5,
- *      https://stackoverflow.com/questions/24098461/nodemailer-gmail-what-exactly-is-a-refresh-token-and-how-do-i-get-one
- * ]
  */
 
+const AWS = require('aws-sdk');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const juice = require('juice');
 
 /**
  * Send a welcome email after a user subscribes
- * @param to - recipient email address
- * @param verify_cd - verification code
- * @param unsub_cd - unsubscribe code
+ * @param to Recipient email address
+ * @param verify_cd Verification code
+ * @param unsub_cd Unsubscribe code
  */
 function sendWelcomeEmail(to="andrew@jarombek.com", verify_cd, unsub_cd) {
 
@@ -34,22 +30,39 @@ function sendWelcomeEmail(to="andrew@jarombek.com", verify_cd, unsub_cd) {
 
     console.info(styledHtml);
 
-    const transport = nodemailer.createTransport({
+    const secretsManager = new AWS.SecretsManager({ region: 'us-east-1' });
+    secretsManager.getSecretValue({ SecretId: 'google-account-secret' }, (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const { SecretString } = data;
+            const secretObject = JSON.parse(SecretString);
+            const password = secretObject.password;
+
+            const transport = createTransport(password);
+            sendMail(transport, to, styledHtml);
+        }
+    });
+}
+
+function createTransport(password) {
+    return nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
-            type: 'OAuth2',
-            clientId: '185890864563-6f8l8d9rn7s2qao51150k9eqt1dpbnr8.apps.googleusercontent.com',
-            clientSecret: 'ducKth5OeJu9_RDQcB7Ug5Ps'
+            user: 'andrew@jarombek.com',
+            pass: password
         }
     });
+}
 
+function sendMail(transport, to, html) {
     transport.sendMail({
         from: 'Andrew Jarombek<andrew@jarombek.com>',
         to,
         subject: 'Andrew Jarombek\'s Software Development Blog Subscription',
-        html: styledHtml,
+        html,
         attachments: [
             {
                 filename: 'blizzard.png',
@@ -66,13 +79,7 @@ function sendWelcomeEmail(to="andrew@jarombek.com", verify_cd, unsub_cd) {
                 path: './assets/jarombek_signature.png',
                 cid: 'signature@jarombek.com'
             }
-        ],
-        auth: {
-            user: 'andrew@jarombek.com',
-            refreshToken: '1/iTjqIxJDBo6b6rZg9sJ5AeF-N-zLnUIexsa-_g0x3w820I1ga-yopF7_6a7E4vJk',
-            accessToken: 'ya29.GlvbBdzLb32bi-2OvaXgwT6xSj_FFdQ1BeEEKD73vxIe4_Hct9Pjy_afnMzXM0kiz5B22jYKqg3CXEScL1iXfhuzUvG41kvrFQk1EPSWmSFvcOJnLcsH3QQcV0FR',
-            expires: 3600
-        }
+        ]
     }, (err) => {
         if (err) {
             console.error(`Something went wrong sending mail: ${err}`);
@@ -82,10 +89,10 @@ function sendWelcomeEmail(to="andrew@jarombek.com", verify_cd, unsub_cd) {
 
 /**
  * Replace a template item in a string.  Templates are denoted by the {{<template>}} pattern
- * @param string - the string with the template items
- * @param substring - the <template> identifier
- * @param replacement - string that will replace the template item
- * @return {Object|string|void|*} - A new string - note strings are immutable so a new string
+ * @param string -The string with the template items
+ * @param substring The <template> identifier
+ * @param replacement String that will replace the template item
+ * @return {Object|string|void|*} A new string - note strings are immutable so a new string
  * object will be made.
  */
 function replace(string, substring, replacement) {
